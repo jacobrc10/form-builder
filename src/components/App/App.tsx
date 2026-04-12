a682f3729ebed537bf9e944dc63648fe0ee0c326
Jacob Romano Carlsen <jromanocarlsen@gmail.com>
2026-04-12 14:50:27 -0700

    Refactored folder structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import reactLogo from '../../../public/react.svg'
+import viteLogo from '../../../public/vite.svg'
+import heroImg from '../../../public/hero.png'
 import './App.css'
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
+          <h2>Testing hot reload</h2>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
