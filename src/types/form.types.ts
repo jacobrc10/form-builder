eacae7213199447ba5f770a7172a40b8b36024e4
Jacob Romano Carlsen <jromanocarlsen@gmail.com>
2026-04-12 15:06:30 -0700

    Added interface for form fields

---
+++ src/types/form.types.ts
@@ -0,0 +1,12 @@
+type FieldType = "text" | "number" | "email" | "password" | "date";
+
+export interface FormField {
+  id: string;
+  name: string;
+  label: string;
+  type: FieldType;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  pattern?: RegExp;
+}
