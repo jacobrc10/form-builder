62a3d50eb9acd072a9cdbd6ae49f0c89016a7f9b
Jacob Romano Carlsen <jromanocarlsen@gmail.com>
2026-04-26 15:38:19 -0700

    Created framework for FormRenderer tests

---
+++ src/components/FormRenderer/FormRenderer.test.tsx
@@ -0,0 +1,24 @@
+import FormRenderer from "./FormRenderer";
+import { describe, it } from "vitest";
+import { render } from "@testing-library/react";
+import { formFields } from "../../constants/formFieldData";
+
+
+describe('FormRenderer', () => {
+  it('should render form fields correctly', () => {
+    // Test implementation goes here
+  });
+
+  it('should render the correct number of fields', () => {
+    // Test implementation goes here'
+  });
+
+  it('should display the correct label for each field', () => {
+    // Test implementation goes here
+  });
+
+  it('should handle an empty fields array', () => {
+    // Test implementation goes here
+  });
+
+});
