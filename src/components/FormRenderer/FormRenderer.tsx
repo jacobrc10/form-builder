e0aad75526d7d28904827be293e2e32704cfc973
Jacob Romano Carlsen <jromanocarlsen@gmail.com>
2026-04-26 16:04:36 -0700

    Added empty fieldData message

--- src/components/FormRenderer/FormRenderer.tsx
+++ src/components/FormRenderer/FormRenderer.tsx
@@ -1,9 +1,8 @@
-import Field from '../Field/Field';
-import type { FormField } from '../../types/form.types';
-import { useState } from 'react';
+import Field from "../Field/Field";
+import type { FormField } from "../../types/form.types";
+import { useState } from "react";
 
-const FormRenderer = ({ fields } : { fields: FormField[] }) => {
-
+const FormRenderer = ({ fields }: { fields: FormField[] }) => {
   const [formData, setFormData] = useState<{ [key: string]: any }>({});
 
   const updateFormData = (name: string, value: any) => {
@@ -15,12 +14,22 @@ const FormRenderer = ({ fields } : { fields: FormField[] }) => {
 
   return (
     <>
-    <button onClick={() => console.log('Form data on submit:', formData)}>Submit</button>
-    <form>
-      {fields.map((field : FormField) => (
-        <Field key={field.id} updateFormData={updateFormData} fieldData={field} />
-      ))}
-    </form>
+      <button onClick={() => console.log("Form data on submit:", formData)}>
+        Submit
+      </button>
+      <form id="form-renderer">
+        {fields.length > 0 ? (
+          fields.map((field: FormField) => (
+            <Field
+              key={field.id}
+              updateFormData={updateFormData}
+              fieldData={field}
+            />
+          ))
+        ) : (
+          <p>No fields to display</p>
+        )}
+      </form>
     </>
   );
 };
