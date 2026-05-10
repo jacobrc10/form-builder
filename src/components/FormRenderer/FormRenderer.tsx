9bb0dabffb7759ddab101df348088be660248d96
Jacob Romano Carlsen <jromanocarlsen@gmail.com>
2026-05-10 16:42:49 -0700

    Adjusted FormRenderer to function with placeholder form field list

--- src/components/FormRenderer/FormRenderer.tsx
+++ src/components/FormRenderer/FormRenderer.tsx
@@ -1,32 +1,41 @@
 import Field from "../Field/Field";
-import { FormFieldSchema, type FormField } from "../../types/form.types";
+import { FormFieldListSchema, type FormField, type FormFieldList } from "../../types/form.types";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const FormRenderer = ({ fields }: { fields: FormField[] }) => {
-
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm<FormField>({
-    resolver: zodResolver(FormFieldSchema),
+  } = useForm<FormFieldList>({
+    resolver: zodResolver(FormFieldListSchema),
   });
 
-  const onSubmit: SubmitHandler<FormField> = (data) => {
+  const onSubmit: SubmitHandler<FormFieldList> = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     console.log("Form data on submit:", data);
-
   };
 
+  const getFieldError = (fieldName: string) => {
+    const error = errors[fieldName as keyof FormFieldList];
+    return error;
+  };
+
+  // Todo - FormFieldList needs to be abstracted since we're making the form dynamically and we won't know the field names ahead of time. 
+  // We can use zod to validate the form data on submit and display errors for any fields that don't match the schema requirements. 
+  // This will allow us to ensure that the form data is valid before it's submitted, even if we don't know the specific field names ahead of time.
+
+  // Will need to adjust types accordingly and probably make a dummy application of the dynamic form field list to test
   return (
     <>
       <form id="form-renderer" onSubmit={handleSubmit(onSubmit)}>
         {fields.length > 0 ? (
           fields.map((field: FormField) => (
             <Field
-              key={field.id}
+              key={field.name}
               register={register}
-              errors={errors}
+              errors={getFieldError(field.name)}
               fieldData={field}
             />
           ))
@@ -34,7 +43,7 @@ const FormRenderer = ({ fields }: { fields: FormField[] }) => {
           <p>No fields to display</p>
         )}
         <button type="submit" disabled={isSubmitting}>
-          Submit
+          {isSubmitting ? "Submitting..." : "Submit"}
         </button>
       </form>
     </>
